cede3c353720c25bdc5eef442bc8973f96956238
LuizaNote <luizabordignonmoro@gmail.com>
2026-03-07 14:20:58 -0300

    form feito

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 const Global = createGlobalStyle`
-    
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap');
+
     * {
-    margin: 0;
-    padding: 0;
-    font-family: 'poppins', 'sans-serif';
+        margin: 0;
+        padding: 0;
+        font-family: 'poppins', 'sans-serif';
     }
 
     body {
